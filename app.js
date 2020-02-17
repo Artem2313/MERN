e648999469c4ba0ce8eaacf930672833033d2890
Artem2313 <huselnykov@gmail.com>
2020-02-17 18:37:07 +0200

    20 minutes

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const PORT = config.get("port");
+
+const app = express();
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}`));
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit(1);
+  }
+};
+
+start();
