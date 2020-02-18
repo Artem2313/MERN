3d6bb38804817b028e53bc9543d6c1002750fa8f
Artem2313 <huselnykov@gmail.com>
2020-02-18 17:44:28 +0200

    2

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
 
-const PORT = config.get("port");
-
 const app = express();
+
+app.use("/api/auth", require("./routes/auth.routes"));
+
+const PORT = config.get("port");
 
 const start = async () => {
   try {
